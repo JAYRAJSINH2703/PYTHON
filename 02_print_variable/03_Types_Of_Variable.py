6306bcb2f847ef1eeba8f3bb3b442ff54643b701
JAYRAJSINH2703 <baradjayrajsinh45@gmail.com>
2026-09-04 20:05:55 +0530

    ariable and type of Variable added

---
+++ 02_print_variable/03_Types_Of_Variable.py
@@ -0,0 +1,82 @@
+Name="Jayrajsinh"
+print(Name)
+
+Age="17"
+print(Age)
+
+Collage="SSCCS"
+print(Collage)
+
+
+print("Types Of Variable In Python")
+
+
+#Here is some Rules of Variable
+
+# 1. Variable name always start with a alphabet 
+# 2. In a Variable name blank space not allowed 
+# 3. In a Variable name special symbol not allowed like (),?,!,{} except _ (underscore)
+# 4. In a Variable name Special Character not allowed like @, #, $, %, ^, &, *, +, =, /, \, |, ~, `, <, > except _ (underscore)
+
+
+# Now the Type Of Variable
+
+# 1. String Variable 
+
+Name="Jayrajsinh"
+print(Name)
+
+# 2. Integer Variable
+
+#This Variable is used to store whole number value (Purnank)
+
+Age=17
+print(Age)
+
+# 3. Float Variable 
+
+#This Variable is used to store Decimal Number value (Apurnank)
+
+Height=174.89
+print("My height is:",Height)
+
+
+# 4. Boolean Variable 
+
+# This Variable is used to show Condition like True or False
+
+
+Age=17
+print(Age > 18) 
+
+print(Age >= 17)
+
+
+# 5. List Variable 
+
+# In This Variable we use this []
+
+# This Variable is used to store multiple Value in a single variable
+
+# In This Variable we can change or add or remove the value from the list 
+
+Fruits=['Mango','banana','Grapes','Apple']
+Fruits[0]='watermelon'
+print(Fruits)
+
+
+# 6. Tuple Variable
+
+# In This Variable we use this ()
+
+# This Varible is used to store multiple value in a single variable
+
+# In This Variable we can't change or add or remove the value from the list
+
+Fruits=('Mango','Banana','Grapes','Apple')
+print(Fruits)
+
+
+
+
+
